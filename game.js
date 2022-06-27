adde81c5f6f0b95646c684849ae294e8a22dce19
Oliver Mu <muluyu1213@gmail.com>
2022-06-27 02:35:32 -0400

    implemented better UI changed the game with buttons

--- game.js
+++ game.js
@@ -4,84 +4,118 @@ function computerPlay(){
     return plays[Math.floor(Math.random()*plays.length)]
 }
 
-function capitalize(str){
-    if(str==null)
-    {
-        return "";
-    }
-    return str.substring(0,1).toUpperCase()+str.substring(1);
-}
-
-function checkResult(input){
-    if(input == "Rock"||input =="Paper"||input=="Scissors"){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-
 function playRound(playerSelection, computerSelection){
-    var playerChoice = capitalize(playerSelection);
     if(computerSelection=="Rock"){
-        if(playerChoice=="Rock"){
+        if(playerSelection=="Rock"){
             return "draw";
-        }else if(playerChoice=="Paper"){
+        }else if(playerSelection=="Paper"){
             return "win";
-        }else if(playerChoice=="Scissors"){
+        }else if(playerSelection=="Scissors"){
             return "lost";
         }
     }else if(computerSelection=="Paper"){
-        if(playerChoice=="Rock"){
+        if(playerSelection=="Rock"){
             return "lost";
-        }else if(playerChoice=="Paper"){
+        }else if(playerSelection=="Paper"){
             return "draw";
-        }else if(playerChoice=="Scissors"){
+        }else if(playerSelection=="Scissors"){
             return "win";
         }
     }
     else if(computerSelection=="Scissors"){
-        if(playerChoice=="Rock"){
+        if(playerSelection=="Rock"){
             return "win";
-        }else if(playerChoice=="Paper"){
+        }else if(playerSelection=="Paper"){
             return "lost";
-        }else if(playerChoice=="Scissors"){
+        }else if(playerSelection=="Scissors"){
             return "draw";
         }
     }
 }
 
-function game(){
-    var playerPoints = 0; 
-    var computerPoints = 0;
-    while(playerPoints < 3 || computerPoints < 3){
-        if(playerPoints == 3 || computerPoints == 3){
+function game(playerChoice){
+    
+    var computerSelection = computerPlay();
+    var result = playRound(playerChoice, computerSelection);
+
+    switch(result){
+        case "win":
+            playerPoints++;
+            gameResult.innerHTML=`You ${result}! ${playerChoice} beats ${computerSelection}`;
+            scoreBoard.innerHTML=`Player Points: ${playerPoints} - Computer Points: ${computerPoints}`;
             break;
-        }
-        do{
-            var playerChoice = capitalize(prompt("Please enter you choice:'Rock' 'Paper' or 'Scissors' "));
-        }while(!checkResult(playerChoice))
-        var computerSelection = computerPlay();
-        var result = playRound(playerChoice, computerSelection);
-        switch(result){
-            case "win":
-                playerPoints++;
-                alert(`You ${result}! ${playerChoice} beats ${computerSelection} \nPlayer Points: ${playerPoints} - Computer Points: ${computerPoints}`) ;
-                break;
-            case "lost":
-                computerPoints++;
-                alert(`You ${result}! ${computerSelection} beats ${playerChoice} \nPlayer Points: ${playerPoints} - Computer Points: ${computerPoints}`);
-                break;
-            case "draw":
-                alert("The game is a draw \nPlayer Points: ${playerPoints} - Computer Points: ${computerPoints}") ;
-                break;
-        }
+        case "lost":
+            computerPoints++;
+            gameResult.innerHTML=`You ${result}! ${computerSelection} beats ${playerChoice}`;
+            scoreBoard.innerHTML=`Player Points: ${playerPoints} - Computer Points: ${computerPoints}`
+            break;
+        case "draw":
+            gameResult.innerHTML=`The game is a draw`;
+            scoreBoard.innerHTML=`Player Points: ${playerPoints} - Computer Points: ${computerPoints}`;
+            break;
     }
     if(playerPoints ==3&&computerPoints!=3){
-        alert("Congradulation, You Won!");
-    }else{
-        alert("Unfortunately, You Lost!");
+        replay.appendChild(replayMessage);
+        replay.appendChild(replayButton);
+        currentState = false;
+
+    }else if(playerPoints!=3&&computerPoints==3){
+        replay.appendChild(replayMessage);
+        replay.appendChild(replayButton);
+        currentState = false;
     }
+    
+    
 }
 
-game();
+function  clearGame(){
+    playerPoints = 0;
+    computerPoints = 0;
+    gameResult.innerHTML = "Please make your choice";
+    scoreBoard.innerHTML = "Player Points: 0 - Computer Points: 0";
+    replay.removeChild(replayButton);
+    replay.removeChild(replayMessage);
+    currentState = true;
+
+}
+
+
+let playerChoice;
+var playerPoints = 0; 
+var computerPoints = 0;
+var currentState = true;
+const rock = document.querySelector('#rock');
+rock.addEventListener('click',() =>{
+    if(currentState){
+        game("Rock");
+    }
+    
+})
+const paper = document.querySelector('#paper');
+paper.addEventListener('click',() =>{
+    if(currentState){
+        game("Paper");
+    }
+})
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click',() =>{
+    if(currentState){
+        game("Scissors");
+    }
+})
+
+
+const gameResult = document.querySelector('.result');
+const scoreBoard = document.querySelector('.score');
+
+const replay = document.querySelector('#final');
+const replayButton = document.createElement('button');
+replayButton.textContent = 'Yes';
+
+const replayMessage = document.createElement('h2');
+replayMessage.textContent = 'The Game is Over, Would you like to play again?';
+
+
+replayButton.addEventListener('click',() =>{
+    clearGame();
+})
